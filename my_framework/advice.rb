c59e5e8d260e99e742532f86e7887b3cb10712c2
Nick Johnson <mienaineko@gmail.com>
2017-05-26 21:44:36 -0700

    added my_framework files

---
+++ my_framework/advice.rb
@@ -0,0 +1,13 @@
+module Advice
+  ADVICE_LIST =
+    ["Look deep into nature, and then you will understand everything better.",         "I have found the paradox, that if you love until it hurts, there can be no more hurt, only more love.",
+     "What we think, we become.",
+     "Love all, trust a few, do wrong to none.",
+     "Oh, my friend, it's not what they take away from you that counts. It's what you do with what you have left.",
+     "Lost time is never found again.",
+     "Nothing will work unless you do."]
+
+  def self.generate
+    ADVICE_LIST.sample
+  end
+end
