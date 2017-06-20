f39c6bc69da0882c12ae2f7c68b7a4a281874994
Nick Johnson <mienaineko@gmail.com>
2017-06-20 13:39:26 -0700

    finished expense CLI app

--- expense/schema.sql
+++ expense/schema.sql
@@ -13,6 +13,20 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
@@ -27,7 +41,7 @@ CREATE TABLE expenses (
     id integer NOT NULL,
     amount numeric(6,2) NOT NULL,
     memo text NOT NULL,
-    created_on timestamp without time zone DEFAULT now() NOT NULL,
+    created_on date NOT NULL,
     CONSTRAINT positive_amount CHECK ((amount >= 0.01))
 );
 
@@ -66,13 +80,15 @@ ALTER TABLE ONLY expenses ALTER COLUMN id SET DEFAULT nextval('expenses_id_seq':
 -- Data for Name: expenses; Type: TABLE DATA; Schema: public; Owner: one
 --
 
+COPY expenses (id, amount, memo, created_on) FROM stdin;
+\.
 
 
 --
 -- Name: expenses_id_seq; Type: SEQUENCE SET; Schema: public; Owner: one
 --
 
-SELECT pg_catalog.setval('expenses_id_seq', 1, false);
+SELECT pg_catalog.setval('expenses_id_seq', 27, true);
 
 
 --
@@ -81,6 +97,16 @@ SELECT pg_catalog.setval('expenses_id_seq', 1, false);
 
 ALTER TABLE ONLY expenses
     ADD CONSTRAINT expenses_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
