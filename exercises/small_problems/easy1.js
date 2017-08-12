d8641eaf40d491da19b1f3db88df4f2004c6bab0
Nick Johnson <mienaineko@gmail.com>
2017-08-11 18:27:02 -0700

    added rest of easy1 exercises

--- exercises/small_problems/easy1.js
+++ exercises/small_problems/easy1.js
@@ -160,7 +160,7 @@
 //   return !(year % 400) || (!(year % 4) && !!(year % 100));
 // }
 
-const isLeapYear = year => !(year % 400) || (!(year % 4) && !!(year % 100));
+// const isLeapYear = year => !(year % 400) || (!(year % 4) && !!(year % 100));
 
 
 // // FE:
@@ -176,23 +176,26 @@ const isLeapYear = year => !(year % 400) || (!(year % 4) && !!(year % 100));
 
 // this will fail when year % 400 === 0
 
-const isLeapYear = year => {
-  return year % 4 === 0 ? year % 100 === 0 ? year % 400 === 0 : true : false;
-}
+// const isLeapYear = year => {
+//   return year % 4 === 0 ? year % 100 === 0 ? year % 400 === 0 : true : false;
+// }
 
-console.log(isLeapYear(2016)   === true);
-console.log(isLeapYear(2015)   === false);
-console.log(isLeapYear(2100)   === false);
-console.log(isLeapYear(2400)   === true);
-console.log(isLeapYear(240000) === true);
-console.log(isLeapYear(240001) === false);
-console.log(isLeapYear(2000)   === true);
-console.log(isLeapYear(1900)   === false);
-console.log(isLeapYear(1752)   === true);
-console.log(isLeapYear(1700)   === false);
-console.log(isLeapYear(1)      === false);
-console.log(isLeapYear(100)    === false);
-console.log(isLeapYear(400)    === true);
+// console.log(isLeapYear(2016)   === true);
+// console.log(isLeapYear(2015)   === false);
+// console.log(isLeapYear(2100)   === false);
+// console.log(isLeapYear(2400)   === true);
+// console.log(isLeapYear(240000) === true);
+// console.log(isLeapYear(240001) === false);
+// console.log(isLeapYear(2000)   === true);
+// console.log(isLeapYear(1900)   === false);
+// console.log(isLeapYear(1752)   === true);
+// console.log(isLeapYear(1700)   === false);
+// console.log(isLeapYear(1)      === false);
+// console.log(isLeapYear(100)    === false);
+// console.log(isLeapYear(400)    === true);
+
+
+
 
 
 
@@ -200,8 +203,24 @@ console.log(isLeapYear(400)    === true);
 // /*---------------- 8 -----------------*/console.log('\n--------- 8 ----------');
 // //=========== Leap Years Part 2 ===========//
 
+// const isLeapYear = year => {
+//   return (year <= 1752 && !(year % 4)) || (!(year % 400) || (!(year % 4) && !!(year % 100)));
+// }
 
 
+// console.log(isLeapYear(2016)   === true);
+// console.log(isLeapYear(2015)   === false);
+// console.log(isLeapYear(2100)   === false);
+// console.log(isLeapYear(2400)   === true);
+// console.log(isLeapYear(240000) === true);
+// console.log(isLeapYear(240001) === false);
+// console.log(isLeapYear(2000)   === true);
+// console.log(isLeapYear(1900)   === false);
+// console.log(isLeapYear(1752)   === true);
+// console.log(isLeapYear(1700)   === true);
+// console.log(isLeapYear(1)      === false);
+// console.log(isLeapYear(100)    === true);
+// console.log(isLeapYear(400)    === true);
 
 
 
@@ -211,9 +230,39 @@ console.log(isLeapYear(400)    === true);
 // /*---------------- 9 -----------------*/console.log('\n--------- 9 ----------');
 // //=========== Multiples of 3 and 5 ===========//
 
+// const multisum = num => {
+//   let sum = 0;
+
+//   for (let i = 1; i <= num; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) sum += i;
+//   }
+
+//   return sum;
+// }
 
 
+// const multisum = num => {
+//   return [...Array(num)].map((_, i) => ++i)
+//                         .filter(n => !(n % 3 && n % 5))
+//                         .reduce((sum, n) => sum + n);
+// }
 
+
+// const multisum = num => {
+//   return [...Array(num)].reduce((sum, _, i) => sum + (!(++i % 3 && i % 5) ? i : 0), 0);
+// }
+
+
+// console.log(multisum(3));      // 3
+// console.log(multisum(5));      // 8
+// console.log(multisum(10));     // 33
+// console.log(multisum(1000));   // 234168
+
+// console.log(multisum(3) ===    3);
+// console.log(multisum(5) ===    8);
+// console.log(multisum(10) ===   33);
+// console.log(multisum(20) ===   98);
+// console.log(multisum(1000) === 234168);
 
 
 
@@ -222,6 +271,22 @@ console.log(isLeapYear(400)    === true);
 // /*---------------- 10 -----------------*/console.log('\n--------- 10 --------');
 // //=========== ASCII String Value ===========//
 
+// const asciiValue = str => {
+//   return str.split('').reduce((sum, char) => sum + char.charCodeAt(0), 0);
+// }
+
+const asciiValue = str => str.split('').reduce((sum, char) => sum + char.charCodeAt(), 0);
 
 
+console.log(asciiValue('Four score'));         // 984
+console.log(asciiValue('Launch School'));      // 1251
+console.log(asciiValue('a'));                  // 97
+console.log(asciiValue(''));                   // 0
+
+console.log('======');
+
+console.log(asciiValue('Four score') ===    984);
+console.log(asciiValue('Launch School') === 1251);
+console.log(asciiValue('a') ===             97);
+console.log(asciiValue('') ===              0);
 
