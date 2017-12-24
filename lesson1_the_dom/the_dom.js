6c40094399a1a0d3b21badb1253b5ae7a001bb88
Nick Johnson <njohnson7@users.noreply.github.com>
2017-12-24 07:33:29 -0800

    added some files

---
+++ lesson1_the_dom/the_dom.js
@@ -0,0 +1,5 @@
+/*
+- " with HTML markup "
+- " we looked at the JavaScript language and how you can leverage its features "
+*/
+//============================================================================//
--- scratch.js
+++ scratch.js
@@ -1,2 +1,36 @@
+/*
+//======================== A Hierarchy of Nodes =========================//
+- " fundamental tenet "
+- " permissiveness; they "
+- " for text modes, "
+- " (e.g. "DOM Level 1") "
+- #4 diagram: "weelky"
+
+//======================== Node properties =========================//
+- double quoted strings
+- inconsistent use of semicolons in code snippets
+
+___
+
+- " though; to find "
+- `= <p class="intro" id="simple">‚Äö√Ñ¬∂</p>`
+- " a String "
+- no whitespace shown for value of text node `t`
+- contradictory: "Elements don't have any textual content, so the nodeValue for Elements is null. We can use a different property when we need the text within an Element: textContent. ... textContent represents all the textual content of an Element."
+- wrong amount of spaces in return value of `> document.querySelector("p").textContent` (and more)
+
+*/
 
 
+p(document.__proto__);
+p(document.__proto__.__proto__);
+p(document.__proto__.__proto__.__proto__);
+p(document.__proto__.__proto__.__proto__.__proto__);
+
+p(HTMLDocument.__proto__);
+p(HTMLDocument.__proto__.__proto__);
+p(HTMLDocument.__proto__.__proto__.__proto__);
+
+body.querySelector('p')
+<p class=​"intro" id=​"simple">​…​</p>​
+a
