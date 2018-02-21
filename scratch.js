5c4d6f0ed37281cfd60927763350f9306653e02b
Nick Johnson <njohnson7@users.noreply.github.com>
2018-02-20 16:24:03 -0800

    updated managing collections to use handlebars

--- scratch.js
+++ scratch.js
@@ -15,10 +15,10 @@
 - code style
 - redo video
 - list item formatting
-
+-----
 - the first video and paragraph don't specify that there should be an internally stored collection of items -- it only talks about the surface level. it would be nice to know all the requirements before attempting the project (for students who do it on their own before looking ahead).
 - "three text inputs, name, stock number, and quantity."
-- rephrase: "the row of inputs for an item are not on the page, and that we've prepared a template (the <script type="text/template" id="inventory_item"> element) that we can use to add them dynamically on the page later."
+- rephrase: "the row of inputs for an item are not on the page, and that we've prepared a template (the `<script type="text/template" id="inventory_item">` element) that we can use to add them dynamically on the page later."
 - "The purpose of the IIFE, together with the global inventory is to say"
 - "after you save away the template string - we'll no longer need it. "
 - "You'll see that the instructor use "
@@ -34,14 +34,51 @@
 - " variables and or functions"
 - "the current provided solution"
 - " explore what could be a possible refactor."
-
+- " while going through this project and reviewed"
+-----
 - yet again, he teaches more incorrect information in the video -- claiming that "returning false in a foreach callback tells it to stop running the loop..."
 - `quantity` is a number to start with, but a string if updated
 - use time element and datetime attribute
 - template element instead of script
 
+//======================== HTML Templating With JavaScript =========================//
+- backticks!!! (it's very hard to read as-is without backticks)
+- code style
+- truthy/falsy instead of true/false
+-----
+- ' inline using style attributes'
+- 'We would have quite a bit of code to write just to fill'
+- ' two rows of 9 table '
+- ' or at best a messy '
+- 'nested each loops.'
+- 'This is where the concept '
+- ' Handlebars look like a property name'
+- ' easy to use'
+- ', or an empty array'
+- ' passed in to'
+- confusing: 'The each block takes the property name of an array on the object that is passed in to the Handlebars template and iterates over it. '
+- 'property on it named "items",'
 
+//======================== Practice Problems: Handlebars Basics =========================//
+- this assignment and previous:
+  - "x-handlebars" vs "x-handlebars-template"
+  - line breaks in arrays: https://github.com/airbnb/javascript#arrays--bracket-newline
+-----
+- backticks
+- code style
+-----
+- "download a copy of Handlebars from their homepage "
+- "used to wrap each tag"
+
+//======================== Assignment: Managing Collections in JavaScript with Handlebars =========================//
+- this assignment uses `type="text/x-handlebars-template"` whereas the previous 2 leave out the `-template` part
+- code style
+- `/>`
+-----
+- `id` and `type` attributes are in opposite order as previous assignments
+- cache `$('#inventory')`
+
+//======================== AJAX Requests =========================//
 
 //================================================================================//
 //==============================================================================//*/
-
