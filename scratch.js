b83bb8d2433524756fd966b106a982456197f64b
Nick Johnson <njohnson7@users.noreply.github.com>
2018-02-27 10:43:13 -0800

    added more feedback

--- scratch.js
+++ scratch.js
@@ -1,6 +1,19 @@
 /*//==============================================================================//
-//========================  =========================//
+//======================== Introduction =========================//
+- ' will spend time writing code to provide the functionality required by a contemporary web application.'
 
+//======================== What to Focus On =========================//
+- 'cycle and the components of each part and how '
+- ' and make requests using'
+- 'data" and be able'
+- ' you see and be able'
+- ' URL, parameters, etc. needed '
+- 'you move on'
+
+//======================== HTTP Review =========================//
+*** see HTTP feedback file ***
+
+//======================== Book: Working with Web APIs =========================//
 
 //================================================================================//
 //==============================================================================//*/
