eef09cf66a96eb3b328f0d6aa7028f8de78b64c3
Nick Johnson <njohnson7@users.noreply.github.com>
2018-03-12 19:31:33 -0700

    added things

--- scratch.js
+++ scratch.js
@@ -1,8 +1,82 @@
 /*//==============================================================================//
+//======================== Assessment Format =========================//
 
+//======================== Study Guide: Written Test =========================//
+- periods after list items
+-----
+- 'The Event Model'
+- 'and Asynchronous Programming, and using'
+- 'Important Topics You Need to Study:'
+- 'by using XHR'
+
+//======================== Test: The DOM, Events, and Ajax =========================//
+- capitalization of "Ajax" is inconsistent
+- '3 hours'
+
+//================================================================================//
+//================================================================================//
+//======================== Exotic Animals =========================//
+- periods after list items
+
+
+animal containing
+Choose any pets/animals of your choice
+for an example
 
 
 
 //================================================================================//
 //==============================================================================//*/
 
+
+
+
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   // TODO
+// };
+
+// // const assert = require("chai").assert;
+
+// describe("zeroFill", function() {
+//   it("Sample Tests", function() {
+//      Test.assertEquals(zeroFuel(50, 25, 2), true);
+//      Test.assertEquals(zeroFuel(100, 50, 1), false);
+//   });
+// });
+
+
+
+
+
+
+
+//============================================================================//
+
+
+//============================================================================//
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
