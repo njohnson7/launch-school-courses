11dafd0614f9907e263565799e8ff87e1f9bf6bb
Nick Johnson <njohnson7@users.noreply.github.com>
2018-04-27 16:16:54 -0700

    fix

--- scratch.js
+++ scratch.js
@@ -1,57 +1,4 @@
 /*//==============================================================================//
-//======================== Assessment Format =========================//
-
-//======================== Study Guide: Written Test =========================//
-- periods after list items
------
-- 'The Event Model'
-- 'and Asynchronous Programming, and using'
-- 'Important Topics You Need to Study:'
-- 'by using XHR'
-
-//======================== Test: The DOM, Events, and Ajax =========================//
-- capitalization of "Ajax" is inconsistent
-- '3 hours'
-
-//======================== Written Assessment =========================//
-- capitalization of `Function`, `Array`, etc
-- minor code style issues
-
-***** probably more...? *****
-
-
-*****************************************************************************************
-************ 239 in general -- didn't check for possible feedback much yet **************
-*****************************************************************************************
-//======================== The Take Home Project =========================//
-***** a few things on instructions page *****
-  - ruby (=> 2)
-  - 'The number after on the top of'
-  - ' (i.e Item 1 - 02/15). '
-  - etc...
-  -
-
-***** a few things in backend code *****
-  - mixed quotes
-  - favicon loading error
-  - documentation 'Retreive all todos.' do
-  - API docs:
-    - '"String : Description of the todo)",'
-    - "201 Created - When todo is deleted"
-    - actually is `null` not `false`: '7  "completed": "Boolean : Completion status of todo. Will default to false for new todo"'
-    - 'PUT /api/todos/{id} ... Updates a todo. Uses key/value pairs sent via POST'
->>>>>>>>> IMPORTANT >>>>>>> server won't accept json, only form-url-encoded (spent 1+ hours testing...): "Request payload can be either json or a query string"
-    - etc...
-
-***** a few things in live version of project *****
-  - completing doesn't work right with button
-
-- "PUT /api/todos/{id}
-
-Updates a todo. Uses key/value pairs sent via POST"
-
-//================================================================================//
-//================================================================================//
 //======================== Exotic Animals =========================//
 - periods after list items
 
@@ -63,4 +10,3 @@ for an example
 
 //================================================================================//
 //==============================================================================//*/
-
