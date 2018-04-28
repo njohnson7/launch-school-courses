9fb6de498d950f4b02f260a58ed01180528641e6
Nick Johnson <njohnson7@users.noreply.github.com>
2018-04-27 19:50:22 -0700

    added more exercises

--- scratch.js
+++ scratch.js
@@ -8,5 +8,22 @@ Choose any pets/animals of your choice
 for an example
 
 
+
+//======================== 4: Tree Slicing =========================//
+- HTML element alignment is off (intentional...?)
+- inconsisitent use of backticks in text
+- periods after list items
+
+
+- "inner most" (x2+...)
+It's similar to slice but
+The end index
+- "the end index which is the inner most child node's id attribute. " ... "The end index doesn't have to be the id of the "inner most" child node as some of the examples suggest."
+
+
+
+
+- "`sliceTree(2, 5)`"
+
 //================================================================================//
 //==============================================================================//*/
