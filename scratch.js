4aef64e0314a489cdb979270aeea2460b0b21da7
Nick Johnson <njohnson7@users.noreply.github.com>
2018-04-27 23:36:39 -0700

    added exercise

--- scratch.js
+++ scratch.js
@@ -10,20 +10,28 @@ for an example
 
 
 //======================== 4: Tree Slicing =========================//
-- HTML element alignment is off (intentional...?)
+- HTML element alignment is off
 - inconsisitent use of backticks in text
 - periods after list items
 
+___
 
 - "inner most" (x2+...)
-It's similar to slice but
-The end index
+- "It's similar to slice but"
+- "The end index"
 - "the end index which is the inner most child node's id attribute. " ... "The end index doesn't have to be the id of the "inner most" child node as some of the examples suggest."
-
-
-
-
 - "`sliceTree(2, 5)`"
+
+//======================== 5: Coloring =========================//
+
+
+- "only positive integers will be provided as arguments " -- (`0` is shown in the image)
+Following are
+are some sample output
+`<title>Tracing the DOM Tree</title>`
+Node.children property
+- not necessary -- `parents` is already an array: "`var parentsArray = Array.prototype.slice.call(parents);`"
+
 
 //================================================================================//
 //==============================================================================//*/
