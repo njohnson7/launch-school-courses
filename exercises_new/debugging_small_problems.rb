11a090386ca6c45c38d6fe7c24740c011a7e5a7a
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-20 14:40:21 -0700

    added more debugging exercises

--- exercises_new/debugging_small_problems.rb
+++ exercises_new/debugging_small_problems.rb
@@ -76,7 +76,7 @@ end
 
 
 
-#----- 4: reverse sentence -----#
+#----- 5: card deck -----#
 def score card
   card.is_a?(Integer) ? card : card == :ace ? 11 : 10
 end
@@ -97,7 +97,7 @@ sum          = deck.values.flat_map { |cards| cards.map &method(:score) }.sum
 
 
 
-#----- 5: reverse sentence -----#
+#----- 6: getting things done -----#
 def move(n, from_array, to_array)
   return if n <= 0
   to_array << from_array.shift
@@ -120,7 +120,7 @@ move(5, todo, done)
 
 
 
-#----- 6: neutralizer -----#
+#----- 7: neutralizer -----#
 def neutralize(sentence)
   words = sentence.split(" ")
   words.dup.each do |word|
@@ -146,7 +146,7 @@ end
 
 
 
-#----- 7: password -----#
+#----- 8: password -----#
 def set_password
   puts 'What would you like your password to be?'
   gets.chomp
@@ -164,7 +164,7 @@ end
 
 
 
-#----- 8: number guessing game -----#
+#----- 9: number guessing game -----#
 def guess_number(max_number, max_attempts, winning_number = [*1..max_number].sample, attempts = 0)
   return if attempts >= max_attempts
 
@@ -183,5 +183,83 @@ def guess_number(max_number, max_attempts, winning_number = [*1..max_number].sam
   end
 end
 
-guess_number(10, 3)
+# guess_number(10, 3)
 
+
+
+
+
+#----- 10: TF-IDF -----#
+# Term frequency - inverse document frequency:
+# A measure of how important a term is to a document in a collection of documents
+# (the importance increases proportionally to the term frequency in the document,
+# but decreases with the frequency of the word across documents)
+def tfidf(term, document, documents)
+  tf(term, document) * idf(term, documents)
+end
+
+# Term frequency (simple version):
+# the number of times a term occurs in a document
+def tf(term, document)
+  document.split(/[\s-]/).count { |word| word.downcase == term }
+end
+
+# Inverse document frequency:
+# measure of how much information the word provides,
+# based on the number of documents in which it occurs
+# (the rarer it is, the more information it provides)
+def idf(term, documents)
+  count = documents.count { |d| tf(term, d).nonzero? }
+  count = 1 if count.zero?
+  Math.log documents.size.fdiv(count)
+end
+
+# Very simple example
+document1 = "Schrödinger's cat is a thought experiment often featured in discussions of the interpretation of quantum mechanics. The Austrian physicist Erwin Schrödinger devised " +
+"it in 1935 as an argument against the Copenhagen interpretation of quantum mechanics, which states that an object in a physical system can simultaneously exist in all possible configurations, " +
+"a state called quantum superposition, and only observing the system forces the object into just one of those possible states. Schrödinger disagreed with this interpretation. In particular, " +
+"quantum superposition could not work with larger objects. As an illustration, he presented a scenario with a cat in a sealed box, whose fate was linked to a subatomic event that may or may not occur. " +
+"In a quantum superposed state of the subatomic particles, the cat would be both alive and dead, until someone opened the box and observed it."
+
+document2 = "The domestic cat is a small, furry, carnivorous mammal. The term cat can, however, also refer to wild cats, which include animals like lions, tigers and leopards. " +
+"Cats are claimed to have a lower social IQ than dogs but can solve more difficult cognitive problems and have a longer-term memory. International Cat Day is celebrated on August 8. " +
+"Famous cats include Schrödinger's cat as well as Pudding and Butterscotch, which occur in some of the Launch School assignments."
+
+document3 = "One of the core values that sets Launch School apart from some other coding schools out there is our emphasis on Mastery-based Learning. If the key to becoming a competent and confident Software Engineer " +
+"is deep understanding of first principles, then the key to acquiring that understanding is through Mastery-based Learning. The core of Mastery-based Learning is replacing time with mastery. There's no graduation, " +
+"but a continual learning journey of micro-improvements. At Launch School, we're not trying to catch a wave or take advantage of a surge in demand. Instead, we're trying to focus on things that'll be useful to you for decades to come, " +
+"such as a systematic problem-solving approach or learning how to deconstruct a programming language or building sound mental representations of how web application work. Everything we're trying to do at " +
+"Launch School is with an eye towards sustainable studying habits and building skills for a long-term career."
+
+documents = [document1, document2, document3]
+
+# The higher the tf-idf score of a term for a document, the more informative
+# it is for that document.
+# E.g. when someone searches for the term 'cat' in your document collection,
+# you want to return document1 and document2, but not document3.
+# For the term 'quantum mechanics', on the other hand, you only want to return document1.
+
+# expected outputs:
+puts tfidf("cat", document1, documents) # ~ 1.2
+puts tfidf("cat", document2, documents) # ~ 1.6
+puts tfidf("cat", document3, documents) # 0
+puts
+
+puts tfidf("quantum", document1, documents) # ~ 5.5
+puts tfidf("quantum", document2, documents) # 0
+puts tfidf("quantum", document3, documents) # 0
+puts
+
+puts tfidf("mastery", document1, documents) # 0
+puts tfidf("mastery", document2, documents) # 0
+puts tfidf("mastery", document3, documents) # ~ 3.3
+puts
+
+puts tfidf("some", document1, documents) # 0
+puts tfidf("some", document2, documents) # ~ 0.4
+puts tfidf("some", document3, documents) # ~ 0.4
+puts
+
+docs = [document2, document3]
+puts tfidf("quantum", document2, docs) # ~ 0.4
+puts tfidf("quantum", document3, docs) # ~ 0.4
